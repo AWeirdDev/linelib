1085d6cb547e61b9f9228904f314bb9b5d835378
AWeirdDev <90096971+AWeirdScratcher@users.noreply.github.com>
2022-11-26 17:03:39 +0800

    main

--- examples/cogs/without_args/main.py
+++ examples/cogs/without_args/main.py
@@ -2,7 +2,7 @@ from linelib import Client
 from my_cogs import MyCog
 
 client = Client("secret", "token")
-client.load_commands(MyCog())
+client.load_commands(MyCog)
 
 @client.event("ready")
 def ready():
