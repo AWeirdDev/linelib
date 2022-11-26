b605a91fb429860c1aabf24845bf3cb00fd163f4
AWeirdDev <90096971+AWeirdScratcher@users.noreply.github.com>
2022-11-26 19:08:59 +0800

    database

---
+++ linelib/database.py
@@ -0,0 +1,11 @@
+"""
+🗂 Temporary Database for LINELIB
+"""
+
+class db(object):
+    handlers = {}
+
+# same pointer
+
+# Note:
+# Using this method might not be the best way to store data temporarily, but this is the way I think it could finish the task
