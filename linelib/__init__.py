8196d46df36a357a233ae7814da067e3849c64b4
AWeirdDev <90096971+AWeirdScratcher@users.noreply.github.com>
2022-11-26 19:07:35 +0800

    init

---
+++ linelib/__init__.py
@@ -0,0 +1,14 @@
+"""
+LINELIB
+~~~~~~~~~~~~~~~~~~~
+🚀 Launch your bot to the next level.
+
+:copyright: (c) 2022 AWeirdDev (AWeirdScratcher)
+:license: MIT, see LICENSE for more details.
+"""
+
+from .http import *
+from .models import *
+#from .console import *
+# ^ deprecated
+from .tools import Long # It's useless however
