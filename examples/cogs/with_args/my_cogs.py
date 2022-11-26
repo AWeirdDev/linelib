349e2d89aa40e5969e034e95bbb5a02b0051a10a
AWeirdDev <90096971+AWeirdScratcher@users.noreply.github.com>
2022-11-26 17:10:16 +0800

    drink water example

--- examples/cogs/with_args/my_cogs.py
+++ examples/cogs/with_args/my_cogs.py
@@ -5,4 +5,7 @@ cmd = Client.Commands.cmd
 class MyCog(Client.Commands, prefix="!"):
   @cmd(name="drink")
   def drink(ctx, times: int, reason: Long << str = "no reason"):
+    # Use 'Long << str' for long strings like a sentence,
+    # Otherwise your sentence might be cut.
+    # Example without Long: "this is important" becomes "this"
     ctx.reply(f"You drank water {times} times.\nReason: {reason}")
