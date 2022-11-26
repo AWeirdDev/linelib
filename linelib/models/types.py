fe9055e6113f9307869890ba4c956703bdcedec9
AWeirdDev <90096971+AWeirdScratcher@users.noreply.github.com>
2022-11-26 19:26:25 +0800

    types

---
+++ linelib/models/types.py
@@ -0,0 +1,25 @@
+# sum cool types
+
+class undefined(object):
+    """
+    Represents an undefined object (or anything).
+    """
+    pass
+
+class ms(float):
+    """
+    Mileseconds.
+    """
+    pass
+
+class Count(object):
+    """
+    Count.
+    """
+    pass
+
+class Demographics(object):
+    """
+    Demographics.
+    """
+    pass
