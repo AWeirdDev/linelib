b6a836a85ae882ec7934cea41f1b11b27c05e718
AWeirdDev <90096971+AWeirdScratcher@users.noreply.github.com>
2022-11-26 19:06:49 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='linelib',
+    version='0.1',
+    packages=['linelib'],
+    license='MIT',
+    description="Launch your LINE bot to the next level.",
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type="text/markdown",
+    author='AWeirdScratcher',
+    author_email = "aweirdscrather@gmail.com",
+    install_requires=[
+        'requests', 'flask'
+    ],
+    keywords = ['line', 'bot', 'line bot', 'sdk'],
+    url="https://github.com/AWeirdScratcher/linelib"
+)
